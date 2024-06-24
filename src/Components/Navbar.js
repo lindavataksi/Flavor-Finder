182b4b1d20730a6d3edbde0ae03a33825a89f13d
lindavataksi <lindavataksi3@gmail.com>
2024-06-24 00:04:15 -0400

    created a responsive navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,53 @@
-import React from "react"
-import {Link, useMatch, useResolvedPath} from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <img className="nav--logo" src="https://cdn-icons-png.flaticon.com/256/1995/1995602.png" alt="Logo" />
-            <h1 className="nav--title">FlavorFinder.</h1>
-            <div className="nav--options"> 
-                <button><CustomLink to="/">Home</CustomLink></button>
-                <button><CustomLink to="/Randomizer">Randomizer</CustomLink></button>
-                <button><CustomLink to="/RecipeSearch">Recipe Search</CustomLink></button>
-                {/* <button><CustomLink to="/Favorites">My Favorites</CustomLink></button> */}
-                <button><CustomLink to="/About">About</CustomLink></button>
-            </div>
-        </nav>
-    )
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <section className="navigation">
+    <nav className="navbar navbar-expand-lg navbar-light bg- #e3f2fd;">
+    <img className="nav--logo" src="https://cdn-icons-png.flaticon.com/256/1995/1995602.png" alt="Logo" />
+      <Link className="navbar-brand" to="/">
+        FlavorFinder.
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        onClick={toggleMenu}
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className={`collapse navbar-collapse ${showMenu ? "show" : ""}`}
+        id="navbarSupportedContent"
+      >
+        <ul className="navbar-nav mr-auto">
+          <CustomLink to="/">Home</CustomLink>
+          <CustomLink to="/Randomizer">Randomizer</CustomLink>
+          <CustomLink to="/RecipeSearch">Recipe Search</CustomLink>
+          <CustomLink to="/About">About</CustomLink>
+        </ul>
+      </div>
+    </nav>
+    </section>
+  );
 }
 
-function CustomLink({to, children,   ...props}) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({path: resolvedPath.pathname, end:true})
-    return(
-        <div className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>
-            {children}
-            </Link>
-        </div>
-    )
+function CustomLink({ to, children }) {
+  return (
+    <li className="nav-item">
+      <Link className="nav-link" to={to}>
+        {children}
+      </Link>
+    </li>
+  );
 }
+
